7bef4b21b5d9e4d43d148f8eb913605f45ab6dfa
Phoenix <anmolkhanna2003@gmail.com>
2023-06-08 14:18:51 +0530

    Core Java Fundamentals (Chapter 3_CodeSnippet_2) - Running Java program as a script. - Trying out a little trick

--- src/Core_Java_PartI_Fundamentals/TestingBinaryNumbers.java
+++ src/Core_Java_PartI_Fundamentals/TestingBinaryNumbers.java
@@ -4,10 +4,13 @@ public class TestingBinaryNumbers {
     public static void main(String...args){
         // Thus you can't use this trick to convert numbers from Binary to Decimal
         int num;
+        byte n = -1;//255 on using the method below!
+        System.out.println(Byte.toUnsignedInt(n));
         Scanner scan=new Scanner(System.in);
         String numberInBinary = scan.nextLine();
         String comNumber="0b"+numberInBinary;
         num=Integer.parseInt(comNumber);
         System.out.println(num);
+
     }
 }
