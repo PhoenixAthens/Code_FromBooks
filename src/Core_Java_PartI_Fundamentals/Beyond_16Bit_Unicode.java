35f6be945c53f999425db7df8a662784d63c47af
Phoenix <anmolkhanna2003@gmail.com>
2023-07-06 14:50:41 +0530

    Core Java Fundamentals (Chapter 3_CodeSnippet_2) - Unicode and Character type

--- src/Core_Java_PartI_Fundamentals/Beyond_16Bit_Unicode.java
+++ src/Core_Java_PartI_Fundamentals/Beyond_16Bit_Unicode.java
@@ -9,5 +9,19 @@ public class Beyond_16Bit_Unicode {
         //U+D800 to U+DBFF for the first code unit, U+DC00 to U+DFFF for the second code unit
         //Basically using the surrogate area
 
+        //To check if character is valid for use as an identifier
+        System.out.println(Character.isJavaIdentifierPart('.'));
+        System.out.println(Character.isJavaIdentifierPart('%'));//because it's a operator
+        System.out.println(Character.isJavaIdentifierPart('$'));
+        System.out.println(Character.isJavaIdentifierPart('@'));//because it's used in annotations
+        System.out.println(Character.isJavaIdentifierPart('+'));//because it's a operator
+        System.out.println(Character.isJavaIdentifierStart('1'));//identifier cannot start with a number
+
+        System.out.println("What's thus: \u0090");
+
+
+
+
+
     }
 }
